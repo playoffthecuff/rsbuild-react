3e68a2bee8c3a85419bdb15385fac5ebe69426b4
playoffthecuff <jmobile@bk.ru>
2025-03-04 15:32:42 +0300

    feat: add css types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Earth1 from "./components/earth1";
+import css from "./styles.module.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -8,12 +9,12 @@ const App = () => {
   const handleDown = () => setCount(count - 1);
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
+      <h1 className={css["color-gray"]}>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
-      <button onClick={handleUp} style={{width: 32}}>+</button>
+      <button onClick={handleUp} style={{width: 32}} type="button">+</button>
       <div>{count}</div>
-      <button onClick={handleDown} style={{width: 32}}>-</button>
-      <Earth1 width={16} height={16}/>
+      <button onClick={handleDown} style={{width: 32}} type="button">-</button>
+      <Earth1 width={32} height={32}/>
     </div>
   );
 };
